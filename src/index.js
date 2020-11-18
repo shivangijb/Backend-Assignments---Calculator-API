96714e191dee578490da5ac181b558c122e20660
shivangijb <37670360+shivangijb@users.noreply.github.com>
2020-11-17 22:50:05 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
-
+console.log("hello");
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
